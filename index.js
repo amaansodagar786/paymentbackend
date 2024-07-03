59eff93cb2214dc5beb0bbe44b69ff01d2ddf009
Amaan Sodagar <sodagaramaan786@gmail.com>
2024-07-03 11:36:59 +0530

    commit

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.post('/create-payment-intent', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  })
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server connected on port ${PORT}`);
